19575e58ffbe397cc2a0dcf2beaaaf724eaec340
Harsh Belekar <harshbelekar63@gmail.com>
2026-06-10 11:53:48 +0530

    Retrieve product prices for a specific date

--- 34-Product Price at a Given Date/solution.sql
+++ 34-Product Price at a Given Date/solution.sql
@@ -1,1 +1,24 @@
+SELECT 
+    product_id,  
+    new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN
+(
+    SELECT product_id, max(change_date)
+    FROM products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+UNION 
 
+SELECT 
+    product_id,
+    10 AS price
+FROM Products
+WHERE (product_id) NOT IN
+(
+    SELECT product_id
+    FROM products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+) ;
